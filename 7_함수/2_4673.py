1c9e9613bff1c80de02803bcc970532daaf3d11a
konlrie <konlpyrie@gmail.com>
2020-01-08 23:58:07 +0900

    konlrie

--- 7_함수/2_4673.py
+++ 7_함수/2_4673.py
@@ -1,10 +1,22 @@
-def solve(n):
+def dn(n):
     inf = n
-    for j in range(len(str(n))):
+    for i in range(len(str(n))):
         inf = inf + int(str(n)[-1])
     return inf
 
-print(solve(33))
+print(dn(33))
 
+def constructor(n):
+    int(dn(n)) = n
+    for i in range(len(str(n))):
+        n = n - int(str(n)[-1])
+    return n
 
-
+print(constructor(39))
+ 
+def selfnumber(n):
+    for i in range(1,n+1):
+        inf = n
+        inf = inf + int(str(n)[-1])
+    constructor = 
+    for j in range(len(str(n)))
