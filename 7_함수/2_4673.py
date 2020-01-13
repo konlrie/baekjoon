153e71e306897038a02f7c7ceb876242de88a497
konlrie <konlpyrie@gmail.com>
2020-01-13 17:32:39 +0900

    konlrie

--- 7_함수/2_4673.py
+++ 7_함수/2_4673.py
@@ -1,24 +1,36 @@
-def dn(n):
-    inf = n
-    for i in range(len(str(n))):
-        inf = inf + int(str(n)[-1])
-    return inf
+dn_set = set()
+for n in range(1,10001):
+    dn = n
+    for i in str(n):
+        dn += int(i)
+    dn_set.add(dn)
+selfnumber_set = set(range(1,10001)) - dn_set
+selfnumber_set = sorted(selfnumber_set)
 
-# print(dn(33))
+for j in range(len(selfnumber_set)):
+    print(selfnumber_set[j])
 
-def constructor(n):
-    int(dn(n)) = n
-    for i in range(len(str(n))):
-        n = n - int(str(n)[-1])
-    return n
 
-# print(constructor(39))
+# def dn(n):
+#     inf = n
+#     for i in range(len(str(n))):
+#         inf = inf + int(str(n)[-1])
+#     return inf
+
+# # print(dn(33))
+
+# def constructor(n):
+#     int(dn(n)) = n
+#     for i in range(len(str(n))):
+#         n = n - int(str(n)[-1])
+#     return n
+
+# # print(constructor(39))
  
-def selfnumber(n):
-    for i in range(1,n+1):
+# def selfnumber(n):
+#     for i in range(1,n+1):
         
-        inf = n
-        inf = inf + int(str(n)[-1])
-    constructor = 
-    for j in range(len(str(n)))
-
+#         inf = n
+#         inf = inf + int(str(n)[-1])
+#     constructor = 
+#     for j in range(len(str(n)))
